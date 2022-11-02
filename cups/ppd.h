f301532076b27633db2541f34c2a4715fa84c431
Michael R Sweet <michael.r.sweet@gmail.com>
2022-11-02 11:38:58 -0400

    Bring back a dummy version of cups/ppd.h for macOS.

---
+++ cups/ppd.h
@@ -0,0 +1,15 @@
+//
+// Dummy PPD header file for CUPS v3.
+//
+// This header works around a bug in the macOS AppKit headers which include
+// the old CUPS PPD header.
+//
+// Copyright © 2021-2022 by OpenPrinting.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+
+#ifndef _CUPS_PPD_H_
+#  define _CUPS_PPD_H_
+#endif // !_CUPS_PPD_H_
