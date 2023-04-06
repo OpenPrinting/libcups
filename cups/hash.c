f6f7abdd9eb2432555c9d8ca2d967bf777a27725
Michael R Sweet <michael.r.sweet@gmail.com>
2023-04-05 20:04:19 -0400

    Fix GNU TLS hash calls.

--- cups/hash.c
+++ cups/hash.c
@@ -288,9 +288,9 @@ hash_data(const char    *algorithm,	// I - Algorithm
       goto too_small;
 
     gnutls_hash_init(&ctx, alg);
-    gnutls_hash_update(&ctx, a, alen);
+    gnutls_hash(&ctx, a, alen);
     if (b && blen)
-      gnutls_hash_update(&ctx, b, blen);
+      gnutls_hash(&ctx, b, blen);
     gnutls_hash_deinit(&ctx, hashtemp, sizeof(hashtemp));
 
     memcpy(hash, hashtemp, hashlen);
