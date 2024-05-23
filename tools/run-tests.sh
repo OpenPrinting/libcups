7f60b10e25b141527989951eb359b04a4fab8274
Michael R Sweet <michael.r.sweet@gmail.com>
2024-05-23 10:07:23 -0400

    Use statically linked executables for tests.

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -8,22 +8,19 @@
 # information.
 #
 
-# Support running with shared libraries as necessary on macOS and Linux...
 cd ..
-DYLD_LIBRARY_PATH="$(pwd)/cups"; export DYLD_LIBRARY_PATH
-LD_LIBRARY_PATH="$(pwd)/cups"; export LD_LIBRARY_PATH
 
 # Run ippeveprinter to provide an endpoint for testing...
 name="Test Printer $(date +%H%M%S)"
 status=0
 
 echo "Running ippeveprinter..."
-CUPS_DEBUG_LOG=test-cups.log CUPS_DEBUG_LEVEL=4 CUPS_DEBUG_FILTER='^(http|_http|ipp|_ipp|cupsDNSSD|cupsDo|cupsGet|cupsSend)' tools/ippeveprinter -vvv -a tools/test.conf "$name" 2>tools/test.log &
+CUPS_DEBUG_LOG=test-cups.log CUPS_DEBUG_LEVEL=4 CUPS_DEBUG_FILTER='^(http|_http|ipp|_ipp|cupsDNSSD|cupsDo|cupsGet|cupsSend)' tools/ippeveprinter-static -vvv -a tools/test.conf "$name" 2>tools/test.log &
 ippeveprinter=$!
 
 # Test the instance...
 echo "Running ippfind + ipptool..."
-tools/ippfind -T 30 --literal-name "$name" --exec tools/ipptool -V 2.0 -tIf examples/document-letter.pdf '{}' examples/ipp-2.0.test \; || status=1
+tools/ippfind-static -T 30 --literal-name "$name" --exec tools/ipptool-static -V 2.0 -tIf examples/document-letter.pdf '{}' examples/ipp-2.0.test \; || status=1
 
 if test $status = 1; then
     echo "Unable to find test printer."
