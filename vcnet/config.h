a6608bbb84b44bbaeb7e1ab74b449678046b4ac9
Michael R Sweet <msweet@msweet.org>
2024-10-18 12:11:58 -0400

    Bump version to 3.0rc3.

--- vcnet/config.h
+++ vcnet/config.h
@@ -97,7 +97,7 @@ typedef unsigned long useconds_t;
 // Version of software...
 //
 
-#define LIBCUPS_VERSION "3.0rc2"
+#define LIBCUPS_VERSION "3.0rc3"
 #define LIBCUPS_VERSION_MAJOR 3
 #define LIBCUPS_VERSION_MINOR 0
 
--- xcode/config.h
+++ xcode/config.h
@@ -17,7 +17,7 @@
 // Version of software...
 //
 
-#define LIBCUPS_VERSION "3.0rc2"
+#define LIBCUPS_VERSION "3.0rc3"
 #define LIBCUPS_VERSION_MAJOR 3
 #define LIBCUPS_VERSION_MINOR 0
 
