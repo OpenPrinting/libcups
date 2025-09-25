d78e425b63cf5b29ad90ab658bf32ee8a1c2e911
Michael R Sweet <msweet@msweet.org>
2025-09-25 17:35:32 -0400

    Some more cupsArray changes.

--- tools/ipptool.c
+++ tools/ipptool.c
@@ -1843,7 +1843,7 @@ do_test(ipp_file_t     *f,		// I - IPP data file
 	  add_stringf(data->errors, "detailed-status-message (text(MAX)) has bad length %u (RFC 8011 section 4.1.6.3).", (unsigned)strlen(detailed_status_message));
       }
 
-      a = cupsArrayNew((cups_array_cb_t)strcmp, NULL, NULL, 0, NULL, NULL);
+      a = cupsArrayNew(_cupsArrayStrcmp, NULL, NULL, 0, NULL, NULL);
 
       for (attrptr = ippGetFirstAttribute(response), group = ippGetGroupTag(attrptr);
 	   attrptr;
@@ -1985,7 +1985,7 @@ do_test(ipp_file_t     *f,		// I - IPP data file
 	      break;
 	}
 
-	exp_errors = cupsArrayNew(NULL, NULL, NULL, 0, (cups_acopy_cb_t)strdup, (cups_afree_cb_t)free);
+	exp_errors = cupsArrayNew(NULL, NULL, NULL, 0, _cupsArrayStrdup, _cupsArrayFree);
 	exp_member = strchr(expect->name, '/') != NULL;
 	exp_pass   = false;
 
@@ -7091,7 +7091,7 @@ with_distinct_values(
   }
 
   // Collect values and determine they are all unique...
-  values = cupsArrayNew((cups_array_cb_t)strcmp, NULL, NULL, 0, (cups_acopy_cb_t)strdup, (cups_afree_cb_t)free);
+  values = cupsArrayNew(_cupsArrayStrcmp, NULL, NULL, 0, _cupsArrayStrdup, _cupsArrayFree);
 
   for (i = 0; i < count; i ++)
   {
