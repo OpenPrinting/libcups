e621d369d18e01e2cfbb40bb0d1ed37dc105c7f9
Michael R Sweet <msweet@msweet.org>
2025-10-31 15:54:56 -0400

    Try fixing Windows test script.

--- vcnet/runtests.bat
+++ vcnet/runtests.bat
@@ -24,6 +24,6 @@ cd %1
 
 start "" cmd /c ".\ippeveprinter.exe -vvv -a ../../../tools/test.conf -n localhost 'Test Printer' >test.log"
 
-.\ippfind.exe -T 30 --literal-name "Test Printer" --exec ipptool.exe -V 2.0 -tIf ../../../examples/document-letter.pdf '{}' ../../../examples/ipp-2.0.test ../../../examples/pwg5100.1.test ../../../examples/pwg5100.2.test ../../../examples/pwg5100.7.test ../../../examples/pwg5100.13.test ';'
+.\ippfind.exe -T 30 --literal-name "Test Printer" --exec ipptool.exe -V 2.0 -tIf ../../../examples/document-letter.pdf '{}' ../../../examples/ipp-2.0.test ../../../examples/pwg5100.1.test ../../../examples/pwg5100.2.test ../../../examples/pwg5100.7.test ../../../examples/pwg5100.13.test ";"
 
 :: taskkill /im ippeveprinter.exe
